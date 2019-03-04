6c1893b058c2a0fca9e7fff0892d9b314fcc7ffa
Lacey Umamoto <46430423+lumamoto@users.noreply.github.com>
2019-03-04 14:31:41 -0800

    Add files via upload

---
+++ data/src/Node.hpp
@@ -0,0 +1,21 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    string id;      // id value inside of Node
+    bool visited;   // flag if Node was visited or not
+    Node* prev;       // previously visited Node
+    int dist;       // distance from root to Node
+    vector<Node*> adj; // vector containing all adjacent Nodes
+
+    Node() : id(0), visited(false), prev(NULL), dist(0), adj() {}
+    Node(string str) : id(str), visited(false), prev(NULL), dist(0), adj() {}
+    
+};
+
+#endif  // NODE_HPP

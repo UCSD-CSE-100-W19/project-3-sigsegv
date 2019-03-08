3ed5b9bf137f70e37533de848a7de2fa5224ee6d
Lacey Umamoto <46430423+lumamoto@users.noreply.github.com>
2019-03-08 12:26:11 -0800

    Has correct output

---
+++ src/src/Graph.hpp
@@ -0,0 +1,48 @@
+//
+// Sunny Lau     cs100wav
+// Lacey Umamoto lumamoto
+// 2/26/19 
+//
+// Graph.hpp
+// CSE 100 Project 3
+
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "Node.hpp"
+
+using namespace std;
+
+class Graph {
+public:
+    // vector containing pointers to all nodes in graph
+    vector<Node*> nodes;
+    
+    // hash map that maps a node's id to a pointer to its node
+    unordered_map<string,Node*> nodeMap;
+    
+    Graph();
+    
+    ~Graph();
+    
+    bool loadFromFile(const char* in_filename);
+    
+    string pathfinder(Node* from, Node* to);
+    
+    vector<Node*> socialgathering(const int k);
+    
+    void addNodes(string from, string to);
+    
+    bool containsNode(string id);
+    
+    Node* getNode(string id);
+    
+    static bool compareDegrees(Node* n1, Node* n2);
+    static bool compareIds(Node* n1, Node* n2);
+    
+};
+
+#endif  // GRAPH_HPP
